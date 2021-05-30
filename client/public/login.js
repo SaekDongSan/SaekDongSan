68dfc74309dfb54b87d6c50fc17e9791115ebb9f
psj <dgymjol@yonsei.ac.kr>
2021-05-30 22:11:19 +0900

    login client

--- client/public/login.js
+++ client/public/login.js
@@ -1,4 +1,8 @@
 //로그인
+
+var port = 3000;
+
+
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
@@ -12,3 +16,27 @@ function signOut() {
         console.log('User signed out.');
     });
 }
+
+var xhr = new XMLHttpRequest();
+xhr.open('POST', 'http://localhost:'+port+'/login');
+xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+xhr.onload = function() {
+    console.log('Signed in as: ' + xhr.responseText);
+};
+xhr.send('idtoken=' + profile.getId());
+
+// const {OAuth2Client} = require('google-auth-library');
+// const client = new OAuth2Client(CLIENT_ID);
+// async function verify() {
+//   const ticket = await client.verifyIdToken({
+//       idToken: token,
+//       audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+//       // Or, if multiple clients access the backend:
+//       //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+//   });
+//   const payload = ticket.getPayload();
+//   const userid = payload['sub'];
+//   // If request specified a G Suite domain:
+//   // const domain = payload['hd'];
+// }
+// verify().catch(console.error);
