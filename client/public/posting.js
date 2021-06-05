e7eaf6cd8c779e54b0ac578e8e5cb5f1a3d56ab1
2020147542 <tmfal5023@yonsei.ac.kr>
2021-06-05 13:37:34 +0900

    post

--- client/public/posting.js
+++ client/public/posting.js
@@ -1,1 +1,29 @@
-console.log(new Date().toISOString().slice(0, 10));
+
+//recommend 여부
+$('.recommend').on('click', (e) => {
+    console.log(e.target.value);
+};
+
+function send() {
+
+    var form = $("review")[0];
+    var formData = new FormData(form);
+
+    $.ajax({
+        cache: false,
+        url: "${pageContext.request.contextPath}/upload", // 요기에
+        processData: false,
+        contentType: false,
+        type: 'POST',
+        data: formData,
+        success: function (data) {
+            var jsonObj = JSON.parse(data);
+        }, // success
+
+        error: function (xhr, status) {
+            alert(xhr + " : " + status);
+        }
+    }); // $.ajax */    }
+
+
+}
